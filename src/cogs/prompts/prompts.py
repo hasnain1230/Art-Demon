056e5ea538c845b473aa5a847c546ea0287949fa
hasnain1230 <hasnain1230@gmail.com>
2021-01-02 22:03:30 -0500

    Added optional role parameter and active_prompt functionality.
    Added the option to pass in a role that will be mentioned. Also, added the functionality to tell if there is an active prompt. In the event that there is an active prompt, the command will yield an error to the Discord channel it was called from and the timer will not be set until it the current task is completed, or the current task is disabled and a new one is created.

--- src/cogs/prompts/prompts.py
+++ src/cogs/prompts/prompts.py
@@ -14,11 +14,12 @@ class Prompts(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.color = int('f03c3c', 16)
+        self.active_prompt = False
 
     @commands.command()
-    async def prompt(self, ctx, *args):
-        if args:
-            if args[0].lower() == 'people':
+    async def prompt(self, ctx, *prompt_type: str):
+        if prompt_type:
+            if prompt_type[0].lower() == 'people':
                 prompts = file_open_read('assets/Prompts/People Prompts.txt')
 
                 embed = discord.Embed(title='People Prompt', description=random.choice(prompts),
@@ -26,7 +27,7 @@ class Prompts(commands.Cog):
                 embed.set_footer(text='Happy Drawing >:)')
 
                 await ctx.channel.send(embed=embed)
-            elif args[0].lower() == 'animal':
+            elif prompt_type[0].lower() == 'animal':
                 prompts = file_open_read('assets/Prompts/Animal Prompts.txt')
 
                 embed = discord.Embed(title='Animal Prompt', description=random.choice(prompts),
@@ -34,7 +35,7 @@ class Prompts(commands.Cog):
                 embed.set_footer(text='Happy Drawing >:)')
 
                 await ctx.channel.send(embed=embed)
-            elif args[0].lower() == 'oc':
+            elif prompt_type[0].lower() == 'oc':
                 OCs = file_open_read('assets/Prompts/OC.txt')
                 response = random.choice(OCs)
                 embed = discord.Embed(title='OC Prompt', description=response, colour=discord.Colour(self.color))
@@ -42,7 +43,7 @@ class Prompts(commands.Cog):
 
                 await ctx.channel.send(embed=embed)
 
-            elif args[0].lower() == 'nature':
+            elif prompt_type[0].lower() == 'nature':
                 nature_prompts = file_open_read('assets/Prompts/Nature Prompts.txt')
                 response = random.choice(nature_prompts)
                 embed = discord.Embed(title='Nature Prompt', description=response, color=discord.Colour(self.color))
@@ -61,23 +62,39 @@ class Prompts(commands.Cog):
 
             await ctx.channel.send(embed=embed)
 
+    '''
+    With this command, I am concerned if the datetime is being handled correctly with the passed in time value. I don't
+    know if there is a better way to properly setup the timer. Additionally, in concern to asynchronous programming, I 
+    am not sure what the best way to set the timer is and if I need to even build in functionality to see if there is an
+    active prompt. Definitely more research needs to be done for sure.  
+    '''
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def dailyprompt(self, ctx, arbitrary_prefix, channel: discord.TextChannel,
-                          time_to_run):  # I'm not sure if this is the best way to do this. I have to do some research. =/
+                          time_to_run, *role: discord.Role):  # I'm not sure if this is the best way to do this. I have to do some research. =/
         if arbitrary_prefix == 'set':
-            await ctx.channel.send('Daily Prompt has been set! [Though it is not fully functional right now.]')
+            if self.active_prompt is False:
+                await ctx.channel.send('Daily Prompt has been set! [Though it is not fully functional right now.]')
 
-            time_zone = pytz.timezone('EST')
+                time_zone = pytz.timezone('EST')
 
-            now = datetime.now(time_zone)
-            time_to_run = f'{str(now.date())} {time_to_run}'
-            time_to_run = datetime.strptime(time_to_run, '%Y-%m-%d %H:%M').astimezone(time_zone)
-            delay = (time_to_run - now).total_seconds()
-            ctx.channel = channel
+                now = datetime.now(time_zone)
+                time_to_run = f'{str(now.date())} {time_to_run}'
+                time_to_run = datetime.strptime(time_to_run, '%Y-%m-%d %H:%M').astimezone(time_zone)
+                print(time_to_run)
+                delay = (time_to_run - now).total_seconds()
+                ctx.channel = channel
 
-            await asyncio.sleep(delay)
-            await self.prompt(ctx)
+                self.active_prompt = True
+                await asyncio.sleep(delay)
+                await self.prompt(ctx)
+                self.active_prompt = False
+
+                if role:
+                    await ctx.channel.send(role[0].mention)
+            else:
+                await ctx.channel.send('There is currently an active dailyprompt. Please disable that one to set a new one!')
+
         else:
             raise CommandNotFound
 
